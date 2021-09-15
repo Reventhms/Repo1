a399129c3d86c91a6b534a3fe87462c9e789e295
Reventh <OMEN@LAPTOP-0F1HGU7A>
2021-09-15 14:52:09 +0530

    modified ticket

--- LtiAirlines/src/main/java/com/lti/model/Ticket.java
+++ LtiAirlines/src/main/java/com/lti/model/Ticket.java
@@ -1,5 +1,29 @@
 package com.lti.model;
 
+import java.time.LocalDate;
+
 public class Ticket {
+	int ticketNo;
+	int seatNo;
+	LocalDate datetime;
+	public int getTicketNo() {
+		return ticketNo;
+	}
+	public void setTicketNo(int ticketNo) {
+		this.ticketNo = ticketNo;
+	}
+	public int getSeatNo() {
+		return seatNo;
+	}
+	public void setSeatNo(int seatNo) {
+		this.seatNo = seatNo;
+	}
+	public LocalDate getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(LocalDate datetime) {
+		this.datetime = datetime;
+	}
+	
 
 }
