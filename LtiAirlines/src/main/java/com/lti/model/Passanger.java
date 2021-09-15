97345374e171cc4a46b67258e0197ff8ff58a330
OMEN <OMEN@LAPTOP-0F1HGU7A>
2021-09-15 13:17:11 +0530

    added passenger from br1

---
+++ LtiAirlines/src/main/java/com/lti/model/Passanger.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Passanger {
+	int passengerId;
+	String name;
+	public int getPassengerId() {
+		return passengerId;
+	}
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
